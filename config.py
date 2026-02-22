603d6f50305416b06c836bd531d3e93e693ee153
itsprince05 <102887695+itsprince05@users.noreply.github.com>
2026-02-22 15:12:49 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# Your API Credentials
+API_ID = 35486723
+API_HASH = "2eff0189a873bb08473f18ef71005830"
+BOT_TOKEN = "8398637498:AAGUD6ncda30MtZ-e8G0P2kBRkT2pfc3bi4"
+
+# The only Group ID from where the bot will execute commands
+ADMIN_GROUP_ID = -1003552827391
